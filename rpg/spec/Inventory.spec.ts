42a3cf5c9cc16e98d68c7a8d1ac2c562a8fe16dd
Michel Vorwieger <Michel.Vorwieger@mediaman.de>
2018-06-07 11:36:10 +0200

    added jasmine and first tests

---
+++ rpg/spec/Inventory.spec.ts
@@ -0,0 +1,40 @@
+import "jasmine";
+import {Inventory} from '../src/Inventory';
+import {mockItems} from '../src/mocks/itemMock';
+
+describe("Inventory::", () => {
+    let inventory: Inventory;
+
+    beforeEach(() => {
+        inventory = new Inventory();
+    })
+
+    it('add() should add items to the items array', function () {
+        inventory.add(mockItems[0]);
+
+        expect(inventory.items).toContain(mockItems[0]);
+        expect(inventory.items).toContain(mockItems[0]);
+    });
+
+    it('removeItem() should remove item given as Input', function () {
+        inventory.add(mockItems[0]);
+        inventory.add(mockItems[1]);
+        inventory.removeItem(mockItems[0]);
+
+        expect(inventory.items).not.toContain(mockItems[0]);
+        expect(inventory.items).toContain(mockItems[1]);
+    });
+
+    it('searchItem() should find items by name', function () {
+        inventory.add(mockItems[0]);
+        inventory.add(mockItems[1]);
+
+        expect(inventory.searchItem(mockItems[0].name)).toBe(mockItems[0]);
+        expect(inventory.searchItem(mockItems[0].name)).not.toBe(mockItems[1]);
+    });
+
+    it('GET items() should return getter for all items', function () {
+        inventory.add(mockItems[0]);
+        expect(inventory.items).toContain(mockItems[0]);
+    });
+});
