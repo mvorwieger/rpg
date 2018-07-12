34ee05e540741a50d346a09b8ab01f2b2864da35
Michel Vorwieger <Michel.Vorwieger@mediaman.de>
2018-07-12 11:49:03 +0200

    fixed duplicate identifiers

--- rpg/src/Inventory.ts
+++ rpg/src/Inventory.ts
@@ -1,25 +1,25 @@
 import {Item} from './items/Item';
 
 export class Inventory {
-    private  items: Item[] = [];
+    private inventoryItems: Item[] = [];
 
     public searchItem(nameOfItem: string): Item {
-        return this. items.find((iterator) => iterator.name === nameOfItem);
+        return this.inventoryItems.find((iterator) => iterator.name === nameOfItem);
     }
 
     public add(item: Item): void {
-        this. items.push(item);
+        this.inventoryItems.push(item);
     }
 
     public removeItem(item: Item): void {
-        this. items = this. items.filter(itemIterator => itemIterator !== item);
+        this.inventoryItems = this.inventoryItems.filter(itemIterator => itemIterator !== item);
     }
 
     public contains(item): boolean {
-        return Boolean(this. items.filter((currentItem: Item) => item.name === currentItem.name).length)
+        return Boolean(this.inventoryItems.filter((currentItem: Item) => item.name === currentItem.name).length)
     }
 
     public get items(): Item[] {
-        return this. items;
+        return this.inventoryItems;
     }
 }
