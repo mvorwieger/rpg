ab2113410eb51d0500464eeedcb36311339795ef
Michel Vorwieger <Michel.Vorwieger@mediaman.de>
2018-07-16 17:37:20 +0200

    refactored code to promises instead of cbs

--- rpg/src/Database/models/ItemService.ts
+++ rpg/src/Database/models/ItemService.ts
@@ -5,30 +5,20 @@ import {ModelToMongooseModelConverter} from './ModelToMongooseModelConverter'
 class ItemService {
     public getIdOfItemInDatabase(item: Item): Promise<IItemModel> {
         return new Promise((resolve, reject) => {
-            ItemModel.findOne({name: ModelToMongooseModelConverter.convertItem(item).name}, (err, res) => {
-                if (err) {
-                    reject(err)
-                    return
-                }
-
-                resolve(res)
-            })
+            ItemModel.findOne({name: ModelToMongooseModelConverter.convertItem(item).name})
+                .then(doc => resolve(doc))
+                .catch(err => console.error(err))
         })
     }
 
     public createItemEntryInDb(item: Item): Promise<IItemModel> {
-        return new Promise(((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             const model = new ItemModel(ModelToMongooseModelConverter.convertItem(item))
-            ItemModel.create(model, (err, res) => {
-                if (err) {
-                    reject(err)
-                    return
-                }
-                resolve(res)
-            })
-        }))
+            ItemModel.create(model)
+                .then(docs => resolve(docs))
+                .catch(err => reject(err))
+        })
     }
-
 }
 
 export const itemService = new ItemService()
--- rpg/src/Database/models/PlayerRepository.ts
+++ rpg/src/Database/models/PlayerRepository.ts
@@ -15,29 +15,27 @@ export class PlayerRepository {
             ModelToMongooseModelConverter.convertPlayer(player)
                 .then(convertedPlayer => {
                     PlayerModel
-                        .findById(this.id, (err, doc) => {
-                            if (err) {
-                                reject('Error on updating Player in db' + err)
-                            }
-                        })
-                        .update(convertedPlayer, err => {
-                            if (err) {
-                                reject('Internal Server Error' + err)
-                            }
-                            resolve()
-                        })
-                }).catch(err => reject(err))
+                        .findById(this.id)
+                        .update(convertedPlayer)
+                        .then(doc => resolve(doc))
+                        .catch(err => reject(err))
+                })
+                .catch(err => reject(err))
         })
     }
 
     public createPlayer(player: Player): Promise<IPlayerModel> {
         return new Promise((resolve, reject) => {
-            ModelToMongooseModelConverter.convertPlayer(player).then((convertedPlayer: Player) => {
-                PlayerModel.create(new PlayerModel(convertedPlayer)).then(doc => {
-                    this.id = doc._id
-                    resolve(doc)
-                }).catch(err => console.error('Error while creating Player', err))
-            })
+            ModelToMongooseModelConverter.convertPlayer(player)
+                .then((convertedPlayer: Player) => {
+                    PlayerModel.create(new PlayerModel(convertedPlayer))
+                        .then(doc => {
+                            this.id = doc._id
+                            resolve(doc)
+                        })
+                        .catch(err => reject(err))
+                })
+                .catch(err => reject(err))
         })
     }
 }
